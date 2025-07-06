f153fac6ac0e464fee8ba2d2099ca7ea68dcd52a
Zaki <mahmedzaki670@gmail.com>
2025-07-06 17:49:55 +0500

    hello world styling

--- react 1 Assignment/react/src/main.jsx
+++ react 1 Assignment/react/src/main.jsx
@@ -1,24 +1,16 @@
 import react from 'react'
 import ReactDom from 'react-dom/client'
+import App from './App'
 
 const divElement=document.getElementById('root')
 
-let listed=()=>{
-  return(
-    <ul>
-      <li>Apple</li>
-      <li>Grape</li>
-    </ul>
-  );
-};
+
 
 
 ReactDom.createRoot(divElement).render(
   <>
-  <h1 >
-    Hello World
-  </h1>
-  <listed/>
   
+  <App/>
+    
   </>
 )
